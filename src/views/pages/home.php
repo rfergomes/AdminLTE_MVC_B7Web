ebe3093b7f56257374ad72c0f12881bc1635e987
rferg <rfergomes@gmail.com>
2021-04-07 20:40:21 -0300

    Atualizado

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -9,13 +9,11 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Fixed Layout</h1>
+                    <h1>Home</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Layout</a></li>
-                        <li class="breadcrumb-item active">Fixed Layout</li>
+                        <li class="breadcrumb-item active"><a href="#">Home</a></li>
                     </ol>
                 </div>
             </div>
@@ -24,14 +22,13 @@
 
     <!-- Main content -->
     <section class="content">
-
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <!-- Default box -->
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Title</h3>
+                            <h3 class="card-title">Título</h3>
 
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -43,11 +40,11 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            Start creating your amazing application!
+                            Conteúdo da aplicação
                         </div>
                         <!-- /.card-body -->
                         <div class="card-footer">
-                            Footer
+                            Rodapé
                         </div>
                         <!-- /.card-footer-->
                     </div>
